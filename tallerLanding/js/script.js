840ff32634778ba7fc55f6de97bd6a9934b15e46
Lucas Busnelli <lucasbusnelli19@gmail.com>
2025-05-23 15:47:32 -0300

    CAMBIOS-13
    Marquee mejorado y padding entre secciones.

--- tallerLanding/js/script.js
+++ tallerLanding/js/script.js
@@ -31,30 +31,3 @@ var input_submit = document.getElementById("enviar");
     console.log("Email:", valor_mail);
   }
 });
-
-/*ANIMACIÓN MARQUEE*/
-function Marquee(selector, speed) {
-    const marquee = document.querySelector(selector);
-    const track = marquee.querySelector('.track');
-    const content = track.innerHTML;
-
-    // Duplicar el contenido para efecto continuo
-    track.innerHTML += ' ' + content;
-
-    let position = 0;
-
-    function animate() {
-      position -= speed;
-      if (Math.abs(position) >= track.scrollWidth / 2) {
-        position = 0;
-      }
-      track.style.transform = `translateX(${position}px)`;
-      requestAnimationFrame(animate);
-    }
-
-    animate();
-  }
-
-  window.addEventListener('load', function () {
-    Marquee('.marquee', 0.5); 
-  });
