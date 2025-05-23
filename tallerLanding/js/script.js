636adcdc0b2a71552508cec2fffec9d7fd89c427
Lucas Busnelli <lucasbusnelli19@gmail.com>
2025-05-23 20:36:56 -0300

    CAMBIOS-14
    asd

--- tallerLanding/js/script.js
+++ tallerLanding/js/script.js
@@ -1,24 +1,25 @@
+
 /*ABRIR Y CERRAR POPUP FORMULARIO*/
+
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll(".show-form").forEach(function(btn) {
   btn.addEventListener("click", function(){
     document.querySelector(".pop-up").classList.add("active");
   });
+
+  var input_nombre = document.getElementById("nombre");
+  var input_email = document.getElementById("mail");
+  var input_submit = document.getElementById("enviar");
+
 });
 
 document.querySelector(".pop-up .close-btn").addEventListener("click", function(){
 document.querySelector(".pop-up").classList.remove("active");
 });
 
-
-/*ENVIAR DATOS DE FORMULARIO*/
-
-var input_nombre = document.getElementById("nombre");
-var input_email = document.getElementById("mail");
-var input_submit = document.getElementById("enviar");
-
 /*SE CLICKEO EL SUBMIIT DEL FORMULARIO*/
-  input_submit.addEventListener("click", enviarForm);
+  var formulario = document.querySelector("form");
+  formulario.addEventListener("submit", enviarForm);
 
   function enviarForm(event) {
     event.preventDefault();
@@ -29,5 +30,10 @@ var input_submit = document.getElementById("enviar");
     console.log("Se envio el formulario");
     console.log("Nombre:", valor_nombre);
     console.log("Email:", valor_mail);
+
+   setTimeout(function() {
+    window.location.href = "feedback.html";
+    }, 1000); // Espera 1 segundo antes de redirigir
   }
 });
+
